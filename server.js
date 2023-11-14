9c1a9a4e8ab55020eb7921e2d84c5a6504e5b7df
mgrajez <monikagrajez@gmail.com>
2023-11-14 11:07:57 +0100

    Render deploy

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on https://stokk-app.onrender.com/api`);
 });
