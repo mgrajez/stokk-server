096ebe9f23ae65bc2c8b5253320913d6637fc02c
mgrajez <monikagrajez@gmail.com>
2023-11-14 16:49:45 +0100

    Add auth route under /api

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,6 +5,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+router.use("/auth", require("./auth.routes"));
 router.use("/photos", require("./photo.routes"));
 router.use("/favourites", require("./favourite.routes"));
 
